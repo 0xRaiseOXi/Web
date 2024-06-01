444ca2cb16ceb7d9e566296d53ccbff42aa64cc9
0xRaiseOXi <maximusnesnescafe@mail.ru>
2024-06-01 18:32:55 +0300

    v2

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ vault_size_CONSTANT = {
     10: 10000000
 }
 
-async def update_tokens_value_vault(id):
+def update_tokens_value_vault(id):
     data = collection.find_one({'_id': id})
     data_user_2 = collection2.find_one({'_id': id})
     last_time_update = data['last_time_update']
@@ -82,8 +82,11 @@ def claim_tokens():
     data = collection.find_one({"_id": id['id']})
 
     added_tokens = update_tokens_value_vault(id['id'])
+    print(added_tokens)
     data['oxi_tokens_value'] += added_tokens
     data['last_time_update'] = time.time()
+    print(data)
+    print(id['id'])
     new_data = collection.replace_one({'_id': id['id']}, data)
 
     return jsonify(new_data)
