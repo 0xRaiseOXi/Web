1dc9bcb4d89b6b01ee0e9bc3ecd34e670d184acd
0xRaiseOXi <maximusnesnescafe@mail.ru>
2024-06-03 16:34:57 +0300

    v5.1

---
+++ templates/test/script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const progressBar = document.getElementById('progress-bar');
+    let percentage = 75; // Укажите процент заполнения здесь (например, 75%)
+
+    setProgress(percentage);
+
+    function setProgress(percentage) {
+        if (percentage < 0) percentage = 0;
+        if (percentage > 100) percentage = 100;
+        progressBar.style.width = percentage + '%';
+    }
+});
